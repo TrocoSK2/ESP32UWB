3507f28b43f22c06fb18ecc760b2ac7607a50923
Andrew C. <81301221+TrocoSK2@users.noreply.github.com>
2024-01-12 17:48:30 +0200

    Blinkin' led on GPIO27 with comments

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: CC0-1.0
  */
-
+/*default headers*/
 #include <stdio.h>
 #include <inttypes.h>
 #include "sdkconfig.h"
@@ -11,43 +11,119 @@
 #include "freertos/task.h"
 #include "esp_chip_info.h"
 #include "esp_flash.h"
+/*Headers which must be tried*/
+#include "sdkconfig.h"
+#include "driver/gpio.h"
+
+/*Led configuration*/
+#define GPIO_Pin_27  (1ULL << 27)
+#define BLINK_GPIO GPIO_NUM_27
+
+/*configuration for the function gpio_config to be called*/
+gpio_config_t gpioConfig = {
+    .pin_bit_mask = GPIO_Pin_27,  // Replace with the actual GPIO pin you want to configure
+    .mode = GPIO_MODE_OUTPUT,    // Replace with the desired mode (e.g., GPIO_MODE_INPUT, GPIO_MODE_OUTPUT)
+    .pull_up_en = GPIO_PULLUP_DISABLE,  // Replace with the desired pull-up configuration
+    .pull_down_en = GPIO_PULLDOWN_ENABLE,  // Replace with the desired pull-down configuration
+    .intr_type = GPIO_INTR_DISABLE  // Replace with the desired interrupt type
+};
+
+
+static uint8_t s_led_state = 0;
+
+
+static void configure_led(void)
+{
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+}
+
+static void blink_led(void)
+{
+    /* Set the GPIO level according to the state (LOW or HIGH)*/
+    gpio_set_level(BLINK_GPIO, s_led_state);
+}
+
+/*starting the program with informations about the microcontroller*/
+void init_for_main(void)
+{
+	printf("Hello world!\n");
+
+	    /* Print chip information */
+	    esp_chip_info_t chip_info;
+	    uint32_t flash_size;
+	    esp_chip_info(&chip_info);
+	    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
+	           CONFIG_IDF_TARGET,
+	           chip_info.cores,
+	           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
+	           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
+	           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+	           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+
+	    unsigned major_rev = chip_info.revision / 100;
+	    unsigned minor_rev = chip_info.revision % 100;
+	    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+	    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+	        printf("Get flash size failed");
+	        return;
+	    }
+
+	    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+	           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+	    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+
+	    for (int i = 10; i >= 0; i--) {
+	        printf("Restarting in %d seconds...\n", i);
+	        vTaskDelay(1000 / portTICK_PERIOD_MS);
+	    }
+	    printf("Restarting now.\n");
+	    fflush(stdout);
+
+	    //esp_restart();
+}
+
+
+void config_pin_34(void)
+{
+
+	/*Trying a function, to see if it can be called succesfully*/
+	esp_err_t  error_check;
+	error_check = gpio_config(&gpioConfig);
+	printf("Let's see what you did here %d   s ",error_check);
+
+	/*Configuring what led to be used*/
+	configure_led();
+
+	/*Blinking led to check everything is ok*/
+	while (1)
+	{
+	        blink_led();
+	        /* Toggle the LED state */
+	        s_led_state = !s_led_state;
+	        vTaskDelay(1000 / portTICK_PERIOD_MS);
+	    }
+/*	if(ESP_OK success == error_check)
+	{
+		printf("Good job mate, you've called your first function");
+	}
+	else{
+		printf("Nice try, more luck next time");
+	}*/
+
+}
 
 void app_main(void)
 {
-    printf("Hello world!\n");
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+	/*Blinking led*/
+	config_pin_34();
 
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
-        return;
-    }
+	/*info init*/
+	//init_for_main();
 
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-
-    //esp_restart();
 
 }
